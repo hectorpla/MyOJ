8b87638a03753340ea01be791c192b4857d4927e
Zhanpeng Liang <hectorilles@gmail.com>
2018-02-12 19:16:12 -0800

    setup for colored cursor; event name refactoring

--- week2/oj-client/src/app/services/collaboration.service.ts
+++ week2/oj-client/src/app/services/collaboration.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 declare var io: any;
+declare var ace: any;
+declare var aceRange: any;
 
 @Injectable()
 export class CollaborationService {
@@ -16,19 +18,32 @@ export class CollaborationService {
 
     this.collaborationSocket.on("connected", msg => console.log(msg));
 
-    this.collaborationSocket.on("change from collaborators", delta => {
+    this.collaborationSocket.on("edition change", delta => {
       delete delta.id
       // console.log('change!!! ' + JSON.stringify(delta))
       editor.lastAppliedChange = delta; // order is critical
       editor.getSession().getDocument().applyDeltas([delta]);
     })
 
+    this.collaborationSocket.on("cursor change", (userId: number, position) => {
+      console.log(`cursor changed in ${userId}, ${JSON.stringify(position)}`);
+      // implementation for colored cursors
+      let {row, col} = position;
+      console.log(ace)
+      let range = new aceRange(row, col, row, col + 1);
+      console.log(range);
+
+      editor.getSession()
+        .addMarker(range,'user' + userId,'unknown-type',true);
+    })
+
     this.collaborationSocket.on("disconnect", () => {
       console.log('rejected from the server');
     })
 
+    // participants change notification
     this._userList$ = new Observable<number[]>(observer => {
-        this.collaborationSocket.on("collaborators change", (users) => {
+        this.collaborationSocket.on("participants change", (users) => {
           observer.next(users)
         })
     })
@@ -38,16 +53,19 @@ export class CollaborationService {
       delete delta.id;
       // console.log(delta, '?==', editor.lastAppliedChange);
       if (JSON.stringify(editor.lastAppliedChange) != JSON.stringify(delta)) {
-        this.broadcastChange(delta);
+        this.collaborationSocket.emit('edition change', delta);
       }
+    });
+
+    editor.getSelection().on("changeCursor", () => {
+      // let range = editor.getSelection().getRange();
+      console.log(editor.getCursorPosition());
+      // overlap with on change (typing triger changeCursor)
+      this.collaborationSocket.emit("cursor change", editor.getCursorPosition());
     });
   }
 
   get userList$() { return this._userList$; }
-
-  broadcastChange(delta: Object) {
-    this.collaborationSocket.emit('edition change', delta);
-  }
 
   disconnect() {
     this.collaborationSocket.disconnect();
--- week2/oj-server/services/editorSocketService.js
+++ week2/oj-server/services/editorSocketService.js
@@ -6,8 +6,8 @@ module.exports = function(io) {
         function broadcastCollabChange() {
             console.log('current session ' + sessionId, sessions[sessionId])
             sessions[sessionId].participants.forEach(sid => {
-                io.to(sid).emit('collaborators change', 
-                                sessions[sessionId].participantNums)
+                io.to(sid).emit('participants change', 
+                                sessions[sessionId].participantIDs)
             })
         }
 
@@ -15,39 +15,51 @@ module.exports = function(io) {
         console.log(`a user connected (in session/problem ${sessionId}), the corresponding socket: id is ${socket.id}`)
         io.to(socket.id).emit('connected', {message:"congrads"})
 
-        const socketId = socket.id
+        
         if (!(sessionId in sessions)) {
             sessions[sessionId] = {
                 participants: [],
-                participantNums: [],
+                participantIDs: [],
                 availableSlots: [1,2,3,4,5,6] // for simplicity
             }
         } else if (sessions[sessionId].availableSlots.length == 0) {
             socket.disconnect(true)
             return
-        }        
-        var session = sessions[sessionId]
+        }
+
+        const session = sessions[sessionId]
+        const socketId = socket.id
+        const userId = session.availableSlots.shift() // one-to-one correspondence to socketId
         session.participants.push(socketId)
-        session.participantNums.push(session.availableSlots.shift())
+        session.participantIDs.push(userId)
         console.log('current clients [' + session.participants + ']')
 
-        broadcastCollabChange()
+        broadcastCollabChange() // nofity every one including himself
 
         socket.on('edition change', (delta) => {
-            session.participants.forEach(sid => {
+            session.participants.forEach((sid, index) => {
                 // console.log(sid, socketId)
                 if (sid === socketId) { return }
-                console.log('to change content in ' + sid)
-                io.to(sid).emit("change from collaborators", delta)
+                console.log('to change content in user ' + session.participantIDs[index])
+                io.to(sid).emit('edition change', delta)
             })
         })
+
+        socket.on('cursor change', (position) => {
+            console.log('cursor changed in user' + userId)
+            session.participants.forEach(sid => {
+                if (sid == socketId) { return } // code repeat
+                io.to(sid).emit('cursor change', userId, position)
+            })
+        })
+
         socket.on('disconnect', () => {
             // remove socket from the session
             index = session.participants.indexOf(socketId)
             if (index >= 0) {
                 session.participants.splice(index, 1)
-                session.availableSlots.push(session.participantNums[index])
-                session.participantNums.splice(index, 1)
+                session.availableSlots.push(session.participantIDs[index])
+                session.participantIDs.splice(index, 1)
                 broadcastCollabChange()
             } else {
                 console.log(`WARNING: expected ${socketId} in ${sessionId}`)
